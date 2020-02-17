5f4a1320185b9c55db7d46a07246a33224c48b81
linlinliu <linlinliu03@gmail.com>
2020-02-17 13:44:47 -0800

    change styling

--- src/index.js
+++ src/index.js
@@ -60,6 +60,7 @@ function logoSelection(location = "all", type = "all") {
         .style("width", "230px")
         .style("left", "48px")
         .style("top", "350px");
+
     let companySprite = companyInfo.append("img")
         .attr("class", "company-sprite");
 
